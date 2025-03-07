afe0c572c5f36b972bc361db7ed39a15c2673b52
Beriholic <beriholic@petalmail.com>
2025-03-07 20:19:41 +0800

    feat: ✨ init hertz project with ping service and build script

---
+++ api_tools.py
@@ -0,0 +1,30 @@
+#!/bin/python
+import os
+
+idl_files={}
+
+def get_thrift_files():
+    id=1
+    for file in os.listdir("idl"):
+        if file.endswith(".thrift"):
+            idl_files[id]=file
+            id+=1
+
+def bootstrap():
+    get_thrift_files()
+    print(idl_files)
+
+    id=input("输入要更新idl文件编号(输入0全部更新)：")
+
+    if(id==0):
+        os.system("hz update -idl ./idl/*.thrift")
+        return
+        
+
+    if(idl_files.get(int(id))):
+        os.system("hz update -idl ./idl/"+idl_files[int(id)])
+    else:
+        print("输入的编号不存在")
+
+bootstrap()
+

9f03eac2eac13a7f7258c5792bf2de096bbaeaa1
Beriholic <beriholic@petalmail.com>
2025-03-08 22:27:39 +0800

    style: :lipstick:(ks): format sql script.

--- dependencies/KS.sql
+++ dependencies/KS.sql
@@ -1,65 +1,36 @@
-CREATE TYPE authority_enum AS ENUM ('USER', 'ADMIN', 'SUPER_ADMIN');
-create table "users"
-(
-    id        bigint             not null
-        constraint user_pk
-            primary key,
-    avatar    text,
-    salt      bytea,
-    password  bytea,
-    name      text,
-    phone     text,
-    authority authority_enum default 'USER' not null
+CREATE TYPE authority_enum AS ENUM('USER', 'ADMIN', 'SUPER_ADMIN');
+
+CREATE TABLE "users" (
+    id BIGINT NOT NULL CONSTRAINT user_pk PRIMARY KEY,
+    avatar TEXT DEFAULT '',
+    salt bytea NOT NULL,
+    PASSWORD bytea NOT NULL,
+    NAME TEXT,
+    phone TEXT UNIQUE,
+    authority authority_enum DEFAULT 'USER' NOT NULL
+  );
+
+CREATE TABLE courses (
+    id BIGINT NOT NULL CONSTRAINT course_pk PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT DEFAULT '',
+    cover TEXT DEFAULT '',
+    ascription BIGINT
 );
 
-alter table "users"
-    owner to root;
-
-create table courses
-(
-    id          bigint not null
-        constraint course_pk
-            primary key,
-    title       text,
-    description text,
-    cover       text,
-    ascription  bigint
+CREATE TABLE videos (
+    id BIGINT NOT NULL CONSTRAINT video_pk PRIMARY KEY,
+    source TEXT,
+    title TEXT NOT NULL,
+    description TEXT DEFAULT '',
+    uploader BIGINT,
+    length INTEGER,
+    cover TEXT NOT NULL,
+    ascription BIGINT CONSTRAINT video_course_id_fk REFERENCES courses
 );
 
-alter table courses
-    owner to root;
-
-create table videos
-(
-    id          bigint not null
-        constraint video_pk
-            primary key,
-    source      text,
-    title       text,
-    description text,
-    uploader    bigint,
-    length      integer,
-    cover       text,
-    ascription  bigint
-        constraint video_course_id_fk
-            references courses
+CREATE TABLE user_course (
+    uid BIGINT NOT NULL CONSTRAINT user_course_user_id_fk REFERENCES "users",
+    cid BIGINT NOT NULL CONSTRAINT user_course_course_id_fk REFERENCES courses,
+    CONSTRAINT user_course_pk PRIMARY KEY (uid, cid)
 );
-
-alter table videos
-    owner to root;
-
-create table user_course
-(
-    uid bigint not null
-        constraint user_course_user_id_fk
-            references "users",
-    cid bigint not null
-        constraint user_course_course_id_fk
-            references courses,
-    constraint user_course_pk
-        primary key (uid, cid)
-);
-
-alter table user_course
-    owner to root;
-

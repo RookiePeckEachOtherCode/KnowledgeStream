4c620bc2f1d615c3ab9e7b0f5c34952245c36f64
Vnollx <1744397695@qq.com>
2025-03-11 20:08:51 +0800

    Standardize the service interface

--- biz/service/course_service.go
+++ biz/service/course_service.go
@@ -2,9 +2,13 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/RookiePeckEachOtherCode/KnowledgeStream/biz/dal/pg/entity"
 	"github.com/RookiePeckEachOtherCode/KnowledgeStream/biz/dal/pg/query"
 	"github.com/RookiePeckEachOtherCode/KnowledgeStream/biz/model/base"
+	"github.com/RookiePeckEachOtherCode/KnowledgeStream/biz/utils"
+	"gorm.io/gorm"
 	"sync"
 )
 
@@ -81,4 +85,110 @@ func (s *CourseService) CourseMembersInfoWithCid(c context.Context, cid int64) (
 		result = append(result, userInfo)
 	}
 	return result, nil
+}
+func (s *CourseService) SelectMyCoursesWithUid(c context.Context, uid int64) ([]*base.CourseInfo, error) {
+	uc := query.UserInCourse
+	courseids, err := uc.WithContext(c).Where(uc.UserID.Eq(uid)).Find()
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, fmt.Errorf("查询所在课程域失败: %w", err)
+	}
+	var result []*base.CourseInfo
+	for _, courseid := range courseids {
+		course, err := CourseServ().CourseInfoWithCid(c, courseid.CourseID)
+		if err != nil {
+			return nil, fmt.Errorf("查询所在课程域信息失败: %w", err)
+		}
+		courseInfo := new(base.CourseInfo)
+		courseInfo.Cid = fmt.Sprintf("%d", course.Cid)
+		courseInfo.Cover = course.Cover
+		courseInfo.Description = course.Description
+		courseInfo.Title = course.Title
+		result = append(result, courseInfo)
+	}
+	return result, nil
+}
+func (s *CourseService) CreateCourseWithUid(c context.Context, id int64, title string, description string, cover string) error {
+	cid, err := utils.NextSnowFlakeId()
+	if err != nil {
+		return err
+	}
+	cc := query.Course
+	course := entity.Course{
+		ID:          *cid,
+		Title:       title,
+		Description: description,
+		Cover:       cover,
+		Ascription:  id,
+	}
+	if err := cc.WithContext(c).Save(&course); err != nil {
+		return fmt.Errorf("创建课程域失败: %w", err)
+	}
+	if err = CourseServ().InviteUserWithCidAndUid(c, *cid, id); err != nil {
+		return fmt.Errorf("课程域初始化失败: %w", err)
+	}
+	return nil
+}
+func (s *CourseService) DeleteCourseWithCid(c context.Context, cid int64) error {
+	cc := query.Course
+	course, err := cc.WithContext(c).Where(cc.ID.Eq(cid)).First()
+	if err != nil {
+		return err
+	}
+	if _, err = cc.WithContext(c).Delete(course); err != nil {
+		return fmt.Errorf("删除课程域失败: %w", err)
+	}
+	return nil
+}
+func (s *CourseService) UpdateCourseWithCid(c context.Context, cid int64, title string, description string, cover string) error {
+	cc := query.Course
+	course, err := cc.WithContext(c).Where(cc.ID.Eq(cid)).First()
+	if err != nil {
+		return err
+	}
+	if title != "" {
+		course.Title = title
+	}
+	if description != "" {
+		course.Description = description
+	}
+	if cover != "" {
+		course.Cover = cover
+	}
+	if err = cc.WithContext(c).Save(course); err != nil {
+		return fmt.Errorf("更新课程域失败: %w", err)
+	}
+	return nil
+}
+func (s *CourseService) InviteUserWithCidAndUid(c context.Context, cid int64, uid int64) error {
+	uc := query.UserInCourse
+	uinc, err := uc.WithContext(c).Where(uc.UserID.Eq(uid), uc.CourseID.Eq(cid)).First()
+	if err == nil {
+		if uinc != nil {
+			return errors.New("该学生已在课程中")
+		}
+	} else if !errors.Is(err, gorm.ErrRecordNotFound) {
+		return fmt.Errorf("查询关联记录失败: %w", err)
+	}
+	ucc := entity.UserInCourse{
+		UserID:   uid,
+		CourseID: cid,
+	}
+	if err = uc.WithContext(c).Save(&ucc); err != nil {
+		return fmt.Errorf("邀请学生加入课程域失败: %w", err)
+	}
+	return nil
+}
+func (s *CourseService) OperateMemberWithCidAndUid(c context.Context, cid int64, uid int64) error {
+	uc := query.UserInCourse
+	uinc, err := uc.WithContext(c).Where(uc.UserID.Eq(uid), uc.CourseID.Eq(cid)).First()
+	if err != nil {
+		return fmt.Errorf("该学生并不在该课程域中: %w", err)
+	}
+	if _, err = uc.WithContext(c).Delete(uinc); err != nil {
+		return fmt.Errorf("删除学生失败: %w", err)
+	}
+	return nil
 }
--- biz/service/video_service.go
+++ biz/service/video_service.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"github.com/RookiePeckEachOtherCode/KnowledgeStream/biz/dal/pg/entity"
 	"github.com/RookiePeckEachOtherCode/KnowledgeStream/biz/dal/pg/query"
 	"github.com/RookiePeckEachOtherCode/KnowledgeStream/biz/model/base"
+	"github.com/RookiePeckEachOtherCode/KnowledgeStream/biz/utils"
 	"gorm.io/gorm"
 	"sync"
 )
@@ -49,4 +51,42 @@ func (s *VideoService) VideoInfoService(c context.Context, vid int64) (*base.Vid
 	result.Cover = video.Cover
 	result.Source = video.Source
 	return result, nil
+}
+func (s *VideoService) DeleteVideoWithVid(c context.Context, vid int64) error {
+	v := query.Video
+	video, err := v.WithContext(c).Where(v.ID.Eq(vid)).First()
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return fmt.Errorf("视频不存在或已被删除")
+		}
+		return fmt.Errorf("查询视频失败: %w", err)
+	}
+	_, err = v.WithContext(c).Delete(video)
+	if err != nil {
+		return fmt.Errorf("删除视频失败: %w", err)
+	}
+	return nil
+}
+func (s *VideoService) UploadVideoWithCidAndUid(c context.Context, uid int64, cid int64, source string, title string, description string, cover string, length int) error {
+	v := query.Video
+	id, err := utils.NextSnowFlakeId()
+	if err != nil {
+		return err
+	}
+	video := entity.Video{
+		ID:          *id,
+		Uploader:    uid,
+		Source:      source,
+		Title:       title,
+		Description: description,
+		Cover:       cover,
+		Ascription:  cid,
+		Length:      length,
+	}
+
+	err = v.WithContext(c).Save(&video)
+	if err != nil {
+		return fmt.Errorf("上传视频失败: %w", err)
+	}
+	return nil
 }

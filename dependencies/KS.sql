6a43211a481c19acde74e823fc4d106591b3c618
lingluosama <xiwulingluo@outlook.com>
2025-03-23 16:24:44 +0800

    update:sqlfile

--- dependencies/KS.sql
+++ dependencies/KS.sql
@@ -8,50 +8,98 @@ DROP SCHEMA IF EXISTS KS CASCADE;
 
 CREATE SCHEMA KS;
 
-CREATE TYPE KS.authority_enum AS ENUM ('USER', 'ADMIN', 'SUPERADMIN');
+create type authority_enum as enum ('USER', 'ADMIN', 'SUPER_ADMIN');
 
-CREATE TABLE KS.users (
-    id        BIGINT                   NOT NULL PRIMARY KEY,
-    avatar    TEXT                     DEFAULT '',
-    salt      BYTEA                    NOT NULL,
-    password  BYTEA                    NOT NULL,
-    name      TEXT,
-    phone     TEXT UNIQUE,
-    authority KS.authority_enum        DEFAULT 'USER' NOT NULL,
-    grade     TEXT
+alter type authority_enum owner to root;
+
+create table users
+(
+    id        bigint                                        not null
+        constraint user_pk
+            primary key,
+    avatar    text,
+    salt      bytea,
+    password  bytea,
+    name      text,
+    phone     text,
+    authority authority_enum default 'USER'::authority_enum not null
 );
 
-CREATE TABLE KS.courses (
-    id          BIGINT  NOT NULL PRIMARY KEY,
-    title       TEXT    NOT NULL,
-    description TEXT    DEFAULT '',
-    cover       TEXT    DEFAULT '',
-    ascription  BIGINT,
-    begin_time  TEXT,
-    end_time    TEXT
+alter table users
+    owner to root;
+
+create table courses
+(
+    id          bigint not null
+        constraint course_pk
+            primary key,
+    title       text,
+    description text,
+    cover       text,
+    ascription  bigint
 );
 
-CREATE TABLE KS.videos (
-    id          BIGINT  NOT NULL PRIMARY KEY,
-    source      TEXT,
-    title       TEXT    NOT NULL,
-    description TEXT    DEFAULT '',
-    uploader    BIGINT,
-    length      INTEGER,
-    cover       TEXT    NOT NULL,
-    ascription  BIGINT  REFERENCES KS.courses(id),
-    upload_time TEXT
+alter table courses
+    owner to root;
+
+create table videos
+(
+    id          bigint not null
+        constraint video_pk
+            primary key,
+    source      text,
+    title       text,
+    description text,
+    uploader    bigint,
+    length      integer,
+    cover       text,
+    ascription  bigint
+        constraint video_course_id_fk
+            references courses
 );
 
-CREATE TABLE KS.user_course (
-    uid BIGINT NOT NULL REFERENCES KS.users(id),
-    cid BIGINT NOT NULL REFERENCES KS.courses(id),
-    PRIMARY KEY (uid, cid)
+alter table videos
+    owner to root;
+
+create table user_course
+(
+    uid bigint not null
+        constraint user_course_user_id_fk
+            references users,
+    cid bigint not null
+        constraint user_course_course_id_fk
+            references courses,
+    constraint user_course_pk
+        primary key (uid, cid)
 );
 
-ALTER SCHEMA KS OWNER TO postgres;
-ALTER TYPE KS.authority_enum OWNER TO postgres;
-ALTER TABLE KS.users OWNER TO postgres;
-ALTER TABLE KS.courses OWNER TO postgres;
-ALTER TABLE KS.videos OWNER TO postgres;
-ALTER TABLE KS.user_course OWNER TO postgres;
+alter table user_course
+    owner to root;
+
+create table notifications
+(
+    id       bigint not null
+        constraint id
+            primary key,
+    cid      bigint,
+    content  text,
+    file     text,
+    favorite integer default 0
+);
+
+alter table notifications
+    owner to root;
+
+create table comments
+(
+    id         bigint,
+    ascription bigint,
+    avatar     text,
+    name       text,
+    content    text,
+    parent     bigint
+);
+
+alter table comments
+    owner to root;
+

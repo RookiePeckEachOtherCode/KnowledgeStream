111b7bf5d164ad58016fe755c64c68e33aa0c483
Vnollx <1744397695@qq.com>
2025-03-23 16:49:16 +0800

    Updating database tables and models

--- dependencies/KS.sql
+++ dependencies/KS.sql
@@ -12,70 +12,6 @@ create type authority_enum as enum ('USER', 'ADMIN', 'SUPER_ADMIN');
 
 alter type authority_enum owner to root;
 
-create table users
-(
-    id        bigint                                        not null
-        constraint user_pk
-            primary key,
-    avatar    text,
-    salt      bytea,
-    password  bytea,
-    name      text,
-    phone     text,
-    authority authority_enum default 'USER'::authority_enum not null
-);
-
-alter table users
-    owner to root;
-
-create table courses
-(
-    id          bigint not null
-        constraint course_pk
-            primary key,
-    title       text,
-    description text,
-    cover       text,
-    ascription  bigint
-);
-
-alter table courses
-    owner to root;
-
-create table videos
-(
-    id          bigint not null
-        constraint video_pk
-            primary key,
-    source      text,
-    title       text,
-    description text,
-    uploader    bigint,
-    length      integer,
-    cover       text,
-    ascription  bigint
-        constraint video_course_id_fk
-            references courses
-);
-
-alter table videos
-    owner to root;
-
-create table user_course
-(
-    uid bigint not null
-        constraint user_course_user_id_fk
-            references users,
-    cid bigint not null
-        constraint user_course_course_id_fk
-            references courses,
-    constraint user_course_pk
-        primary key (uid, cid)
-);
-
-alter table user_course
-    owner to root;
-
 create table notifications
 (
     id       bigint not null
@@ -103,3 +39,76 @@ create table comments
 alter table comments
     owner to root;
 
+create table users
+(
+    id        bigint                                        not null
+        constraint user_pk
+            primary key,
+    avatar    text,
+    salt      bytea,
+    password  bytea,
+    name      text,
+    phone     text,
+    authority authority_enum default 'USER'::authority_enum not null,
+    grade     text,
+    class     text,
+    faculty   text,
+    major     text,
+    signature text
+);
+
+alter table users
+    owner to root;
+
+create table courses
+(
+    id          bigint not null
+        constraint course_pk
+            primary key,
+    title       text,
+    description text,
+    cover       text,
+    ascription  bigint,
+    begin_time  text,
+    end_time    text,
+    major       text,
+    class       text
+);
+
+alter table courses
+    owner to root;
+
+create table videos
+(
+    id          bigint not null
+        constraint video_pk
+            primary key,
+    source      text,
+    title       text,
+    description text,
+    uploader    bigint,
+    length      integer,
+    cover       text,
+    ascription  bigint
+        constraint video_course_id_fk
+            references courses,
+    chapter     text
+);
+
+alter table videos
+    owner to root;
+
+create table user_course
+(
+    uid bigint not null
+        constraint user_course_user_id_fk
+            references users,
+    cid bigint not null
+        constraint user_course_course_id_fk
+            references courses,
+    constraint user_course_pk
+        primary key (uid, cid)
+);
+
+alter table user_course
+    owner to root;

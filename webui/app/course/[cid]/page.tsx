3e7545bbdf8a9a7f23a662fdda5ab47bf2d6bc66
Beriholic <17323911768@163.com>
2025-04-19 20:29:18 +0800

    fix: :bug:(course): fix import errors, unused vars, dup definitions

--- webui/app/course/[cid]/page.tsx
+++ webui/app/course/[cid]/page.tsx
@@ -16,8 +16,6 @@ import {IconButton} from "@/app/components/icon-button";
 import {OssBuckets, useOss} from "@/context/oss-uploader-provider";
 
 import { CourseDataVO, fetchCourseData } from "../vo";
-import { useModal } from "@/context/modal-provider";
-import { IconButton } from "@/app/components/icon-button";
 
 export default function CoursePage({
     params,
@@ -294,7 +292,6 @@ function CreateNotificationForm({ cid }: CreateNotificationFormProps) {
     const BYTES_PER_MB = 1024 * 1024;
     const {ossHandleUploadFile, generateSignedUrl} = useOss();
     const [uploadProgress, setUploadProgress] = useState(0); // 新增进度状态
-    var {showNotification} = useNotification();
 
     const { showNotification } = useNotification();
 
@@ -432,115 +429,3 @@ function CreateNotificationForm({ cid }: CreateNotificationFormProps) {
 
 }
 
-
-interface CourseDataVO {
-    id: string,
-    name: string,
-    techer: {
-        id: string,
-        name: string
-        avatar: string
-        signatrue: string
-    },
-    list: Array<{
-        id: string,
-        section_number: string,
-        section_name: string,
-        video_id: string
-    }>
-}
-
-interface RequestType {
-    base: {
-        code: number,
-        msg: string
-    },
-    id: string,
-    name: string,
-    techer: {
-        id: string,
-        name: string,
-        avatar: string,
-        signatrue: string
-    },
-    list: Array<{
-        id: string,
-        section_number: string,
-        section_name: string,
-        video_id: string
-    }>
-}
-
-async function fetchCourseData(cid: string): Promise<RequestType> {
-    const courseCapt = await api.courseService.videos({cid});
-    const courseInfo = await api.courseService.info({cid});
-
-    if (courseCapt.base.code !== 200 || courseInfo.base.code !== 200) {
-        const resp = {
-            base: {
-                code: 0,
-                msg: ""
-            },
-            id: "",
-            name: "",
-            techer: {
-                id: "",
-                name: "",
-                avatar: "",
-                signatrue: ""
-            },
-            list: []
-        }
-        if (courseCapt.base.code !== 200) {
-            resp.base.code = courseCapt.base.code
-            resp.base.msg = courseCapt.base.msg
-        } else if (courseInfo.base.code !== 200) {
-            resp.base.code = courseInfo.base.code
-            resp.base.msg = courseInfo.base.msg
-        }
-        return resp
-    }
-
-    const capts = courseCapt.videosinfo.map((item) => {
-        return {
-            id: item.vid,
-            section_number: item.chapter,
-            section_name: item.title,
-            video_id: item.vid
-        }
-    });
-    const techerInfo = await api.userService.uidInfo({uid: courseInfo.courseinfo.ascription});
-    if (techerInfo.base.code !== 200) {
-        return {
-            base: {
-                code: techerInfo.base.code,
-                msg: techerInfo.base.msg
-            },
-            id: "",
-            name: "",
-            techer: {
-                id: "",
-                name: "",
-                avatar: "",
-                signatrue: ""
-            },
-            list: []
-        }
-    }
-
-    return {
-        base: {
-            code: 200,
-            msg: "success"
-        },
-        id: techerInfo.userinfo.uid,
-        name: courseInfo.courseinfo.title,
-        techer: {
-            id: techerInfo.userinfo.uid,
-            name: techerInfo.userinfo.name,
-            avatar: techerInfo.userinfo.avatar,
-            signatrue: techerInfo.userinfo.signature
-        },
-        list: capts
-    }
-}
